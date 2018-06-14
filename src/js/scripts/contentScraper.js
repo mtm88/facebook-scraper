544a3c617917c4e5bc7a1f6d22dc92bc1619b9c9
Michal <taramichal@gmail.com>
2018-06-14 17:33:15 +0100

    additional parsed values from posts

--- src/js/scripts/contentScraper.js
+++ src/js/scripts/contentScraper.js
@@ -12,10 +12,13 @@
       const author = header.children[0].children[0].children[0].children[0].textContent;
       const timeAdded = header.children[1].children[2].children[0].children[0].children[0].getAttribute("title");
       const title = post.children[0].children[1].children[2].children[0].textContent;
+      const link = post.children[0].children[1].children[3].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[1].getAttribute("href")
+      const contentId = post.children[1].children[0].children[1].getAttribute("value");
+      const shares = parseInt(divsWithPost[0].children[1].children[0].children[3].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].textContent.replace(/\D+/g, ''), 10);
 
-      return parsedPosts.push({ title, author, timeAdded });
+      return parsedPosts.push({ title, author, timeAdded, contentId, shares });
     } catch (error) {
-      return console.error(error);
+      console.error(error);
     }
   });
 
