5377c5413b91ce8d6cb5407256db86a4dfad7c52
Michal <taramichal@gmail.com>
2018-06-29 10:49:34 +0100

    adjusted types for comment properties

--- src/js/scripts/publishPosts.js
+++ src/js/scripts/publishPosts.js
@@ -94,9 +94,9 @@ function parseCommentModels({ commentsContent = [] }, selectedPageDetails) {
 
 		const textType = "text/plain";
 		commentModel.addField("Author", comment.author, 0, textType);
-		commentModel.addField("Link", comment.link, 1, textType);
-		commentModel.addField("Image", comment.authorImage, 2, textType);
-		commentModel.addField("Comment", comment.commentBody, 3, textType);
+		commentModel.addField("Link", comment.link, 1, "text/html");
+		commentModel.addField("Image", comment.authorImage, 2, "image/uri");
+		commentModel.addField("Comment", comment.commentBody, 2, textType);
 
 		return JSON.stringify(commentModel.parsedData);
 	});
