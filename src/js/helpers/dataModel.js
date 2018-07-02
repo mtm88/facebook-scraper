a525aff4240c82fc9e454030aa6dfdda312e6a39
Michal <taramichal@gmail.com>
2018-07-02 10:14:28 +0100

    temp change to API key to not query QA

--- src/js/helpers/dataModel.js
+++ src/js/helpers/dataModel.js
@@ -10,8 +10,8 @@ class DataModel {
 	parseDataForPublish() {
 		this.parsedData = {
 			...this.parsedData,
-			ApiKey: "D85260D6-44AA-46A2-9BA6-6D7AA9F6CB97",
-			// ApiKey: "794467c0-5ca5-437a-acec-93838c4b352b",
+			// ApiKey: "D85260D6-44AA-46A2-9BA6-6D7AA9F6CB97", // QA
+			ApiKey: "794467c0-5ca5-437a-acec-93838c4b352b", // SOC2
 			// ApiKey: this.selectedPageDetails..settings.collectionTokens[0],
 			contentID: this.data.contentId,
 			contentType: "qacovertests.facebook.page.ca93e563-30e3-4581-8bc1-7089d3f63016",
